f56bd3c0aa08927193350ae6605dcc57d00b83cf
ywang_octopus <yijwang@student.ethz.ch>
2021-12-06 23:53:54 +0100

    Add 2D and 3D diffusion solver

--- scripts-part1/Diffusion_2D.jl
+++ scripts-part1/Diffusion_2D.jl
@@ -10,10 +10,8 @@ using Plots, Printf, ImplicitGlobalGrid
 import MPI
 
 # macros to avoid array allocation
-macro qx(ix,iy)  esc(:( -D_dx*(H[$ix+1,$iy+1] - H[$ix  ,$iy+1]) )) end
-macro qy(ix,iy)  esc(:( -D_dy*(H[$ix+1,$iy+1] - H[$ix+1,$iy  ]) )) end
-macro _qx(ix,iy)  esc(:( -D_dx*(Hτ[$ix+1,$iy+1] - Hτ[$ix  ,$iy+1]) )) end
-macro _qy(ix,iy)  esc(:( -D_dy*(Hτ[$ix+1,$iy+1] - Hτ[$ix+1,$iy  ]) )) end
+macro qx(ix,iy)  esc(:( -D_dx*(Hτ[$ix+1,$iy+1] - Hτ[$ix  ,$iy+1]) )) end
+macro qy(ix,iy)  esc(:( -D_dy*(Hτ[$ix+1,$iy+1] - Hτ[$ix+1,$iy  ]) )) end
 
 @parallel_indices (ix,iy) function compute_pseudo!(Hτ2, Hτ, H, D_dx, D_dy, dt, dτ, _dx, _dy, size_H1_2, size_H2_2)
     if (ix<=size_H1_2 && iy<=size_H2_2)
@@ -25,13 +23,6 @@ end
 @parallel_indices (ix,iy) function compute_residuals!(R_H, Hτ, H, D_dx, D_dy, dt, _dx, _dy, size_H1_2, size_H2_2)
     if (ix<=size_H1_2 && iy<=size_H2_2)
         R_H[ix,iy] = -(Hτ[ix+1,iy+1] - H[ix+1,iy+1])/dt - (@qx(ix+1,iy)-@qx(ix,iy))*_dx - (@qy(ix,iy+1)-@qy(ix,iy))*_dy 
-    end
-    return
-end
-
-@parallel_indices (ix,iy) function compute_physical!(H2, H, Hτ, D_dx, D_dy, dt, _dx, _dy, size_H1_2, size_H2_2)
-    if (ix<=size_H1_2 && iy<=size_H2_2)
-        H2[ix+1,iy+1] = H[ix+1,iy+1] - dt*( (@_qx(ix+1,iy) - @_qx(ix,iy))*_dx + (@_qy(ix,iy+1) - @_qy(ix,iy))*_dy )
     end
     return
 end
@@ -50,27 +41,23 @@ end
     tol    = 1e-8
 
     # Derived numerics
-    me, dims = init_global_grid(nx, ny, 1)  # Initialization of MPI and more...
-    @static if USE_GPU select_device() end   # select one GPU per MPI local rank (if >1 GPU per node)
-    dx, dy     = Lx/nx_g(), Ly/ny_g()
-    xc, yc     = LinRange(dx/2, Lx-dx/2, nx), LinRange(dy/2, Ly-dy/2, ny)
-    _dx, _dy   = 1.0/dx, 1.0/dy
+      me, dims = init_global_grid(nx, ny, 1)  # Initialization of MPI and more...
+    @static if USE_GPU select_device() end    # select one GPU per MPI local rank (if >1 GPU per node)
+      dx,   dy = Lx/nx_g(), Ly/ny_g()
+     _dx,  _dy = 1.0/dx, 1.0/dy
     D_dx, D_dy = D/dx, D/dy
-    dτ         = min(dx, dy)^2/D/2.1
-    nt         = cld(ttot, dt)
+            dτ = min(dx, dy)^2/D/4.5
+            nt = cld(ttot, dt)
     nx_v, ny_v = (nx-2)*dims[1], (ny-2)*dims[2] # global dimensions
 
     # Array initialisation
     H   = @zeros(nx, ny)
     H  .= Data.Array([exp(-(x_g(ix,dx,H)+dx/2 -Lx/2)^2-(y_g(iy,dy,H)+dy/2 -Ly/2)^2) for ix=1:size(H,1), iy=1:size(H,2)])
-    H2  = copy(H)
     Hτ  = copy(H)
     Hτ2 = copy(H)
     size_H1_2, size_H2_2 = size(H,1)-2, size(H,2)-2
-    R_H = @ones(nx-2, ny-2) # residual without halo
-    R_v = ones(nx_v, ny_v)  # global residual
-    R_m = maximum(R_v)      # maximum residual
-    err = []
+    R_H = @zeros(nx-2, ny-2) # residual without halo
+    R_v = zeros(nx_v, ny_v)  # global residual
 
     # Preparation of visualisation
     if do_visu
@@ -84,26 +71,27 @@ end
     for it = 1:nt 
         if (it==11) t_tic = Base.time(); niter = 0 end
         # pseudo time stepping
+        R_m = 1.0; iter = 0
         while R_m > tol
             @parallel compute_pseudo!(Hτ2, Hτ, H, D_dx, D_dy, dt, dτ, _dx, _dy, size_H1_2, size_H2_2)
             Hτ, Hτ2 = Hτ2, Hτ
             update_halo!(Hτ)
             # compute residuals
             @parallel compute_residuals!(R_H, Hτ, H, D_dx, D_dy, dt, _dx, _dy, size_H1_2, size_H2_2); gather!(R_H, R_v) 
-            R_m = maximum(R_v)
+            R_m  = maximum(R_v) 
+            iter += 1
+            # if iter % 10 == 0
+            #     println("time = $(round(it*dt, sigdigits=3)), iterations = $(iter), residual = $(R_m)")
+            # end
         end
-
-        @parallel compute_physical!(H2, H, Hτ, D_dx, D_dy, dt, _dx, _dy, size_H1_2, size_H2_2)
-        H, H2 = H2, H
-        update_halo!(H)
-
-        niter += 1
+        # update H 
+        H[:,:] = Hτ; niter += 1
         # Visualize
         if do_visu && (it % nout == 0)
             H_inn .= H[2:end-1,2:end-1]; gather!(H_inn, H_v)
             if (me==0)
                 opts = (aspect_ratio=1, xlims=(Xi_g[1], Xi_g[end]), ylims=(Yi_g[1], Yi_g[end]), clims=(0.0, 1.0), c=:davos, xlabel="Lx", ylabel="Ly", title="time = $(round(it*dt, sigdigits=3))")
-                heatmap(Xi_g, Yi_g, Array(H_v)'; opts...)
+                display(heatmap(Xi_g, Yi_g, Array(H_v)'; opts...))
             end
         end
     end
--- scripts-part1/Diffusion_3D.jl
+++ scripts-part1/Diffusion_3D.jl
@@ -10,9 +10,9 @@ using Plots, Printf, ImplicitGlobalGrid
 import MPI
 
 # macros to avoid array allocation
-macro qx(ix,iy,iz)  esc(:( -D_dx*(C[$ix+1,$iy+1,$iz+1] - C[$ix  ,$iy+1,$iz+1]) )) end
-macro qy(ix,iy,iz)  esc(:( -D_dy*(C[$ix+1,$iy+1,$iz+1] - C[$ix+1,$iy  ,$iz+1]) )) end
-macro qz(ix,iy,iz)  esc(:( -D_dz*(C[$ix+1,$iy+1,$iz+1] - C[$ix+1,$iy+1,$iz  ]) )) end
+macro qx(ix,iy,iz)  esc(:( -D_dx*(Hτ[$ix+1,$iy+1,$iz+1] - Hτ[$ix  ,$iy+1,$iz+1]) )) end
+macro qy(ix,iy,iz)  esc(:( -D_dy*(Hτ[$ix+1,$iy+1,$iz+1] - Hτ[$ix+1,$iy  ,$iz+1]) )) end
+macro qz(ix,iy,iz)  esc(:( -D_dz*(Hτ[$ix+1,$iy+1,$iz+1] - Hτ[$ix+1,$iy+1,$iz  ]) )) end
 
 @parallel_indices (ix,iy,iz) function compute_pseudo!(Hτ2, Hτ, H, D_dx, D_dy, D_dz, dt, dτ, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
     if (ix<=size_H1_2 && iy<=size_H2_2 && iz<=size_H3_2)
@@ -22,62 +22,64 @@ macro qz(ix,iy,iz)  esc(:( -D_dz*(C[$ix+1,$iy+1,$iz+1] - C[$ix+1,$iy+1,$iz  ]) )
     return
 end
 
-@parallel_indices (ix,iy,iz) function compute_physical!(H2, H, D_dx, D_dy, D_dz, dt,  _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
-if (ix<=size_H1_2 && iy<=size_H2_2 && iz<=size_H3_2)
-    H2[ix+1,iy+1,iz+1] = H[ix+1,iy+1,iz+1] - dt*( (@qx(ix+1,iy) - @qx(ix,iy))*_dx + (@qy(ix,iy+1) - @qy(ix,iy))*_dy )
+@parallel_indices (ix,iy,iz) function compute_residuals!(R_H, Hτ, H, D_dx, D_dy, D_dz, dt, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
+    if (ix<=size_H1_2 && iy<=size_H2_2 && iz<=size_H3_2)
+        R_H[ix,iy,iz] = -(Hτ[ix+1,iy+1,iz+1] - H[ix+1,iy+1,iz+1])/dt 
+                        - (@qx(ix+1,iy,iz)-@qx(ix,iy,iz))*_dx - (@qy(ix,iy+1,iz)-@qy(ix,iy,iz))*_dy - (@qz(ix,iy,iz+1)-@qz(ix,iy,iz))*_dz
+    end
     return
 end
 
 @views function diffusion_3D(; do_visu=false)
     # Physics
-    lx, ly, lz = 10.0, 10.0, 10.0 # domain size
+    Lx, Ly, Lz = 10.0, 10.0, 10.0 # domain size
     D          = 1.0              # diffusion coefficient
     ttot       = 1.0              # total simulation time
     dt         = 0.2              # physical time step
 
     # Numerics
-    nx, ny, nz  = 32, 32, 32 
-    nout    = 1
+    nx, ny, nz = 32, 32, 32
+    nout       = 1
+    tol        = 1e-8
 
     # Derived numerics
     me, dims = init_global_grid(nx, ny, nz)  # Initialization of MPI and more...
     @static if USE_GPU select_device() end   # select one GPU per MPI local rank (if >1 GPU per node)
-
-    dx, dy, dz       = Lx/nx_g(), Ly/ny_g(), Lz/nz_g()
-    xc, yc, zc       = LinRange(dx/2, Lx-dx/2, nx), LinRange(dy/2, Ly-dy/2, ny), LinRange(dz/2, Lz-dz/2, nz)
-    _dx, _dy, _dz    = 1.0/dx, 1.0/dy, 1.0/dz
+      dx,   dy,   dz = Lx/nx_g(), Ly/ny_g(), Lz/nz_g()
+     _dx,  _dy,  _dz = 1.0/dx, 1.0/dy, 1.0/dz
     D_dx, D_dy, D_dz = D/dx, D/dy, D/dz
-
-    dτ = min(dx, dy, dz)^2/D/2.1
-    nt = cld(ttot, dt)
+    nx_v, ny_v, nz_v = (nx-2)*dims[1], (ny-2)*dims[2], (nz-2)*dims[3]  # global dimensions
+                  dτ = min(dx, dy, dz)^2/D/4.5
+                  nt = cld(ttot, dt)
     
     # Array initialisation
     H   = @zeros(nx, ny, nz)
     H  .= Data.Array([exp(-(x_g(ix,dx,H)+dx/2 -Lx/2)^2-(y_g(iy,dy,H)+dy/2 -Ly/2)^2-(z_g(iz,dz,H)+dz/2 -Lz/2)^2) for ix=1:size(H,1), iy=1:size(H,2), iz=1:size(H,3)])
-    H2  = copy(H)
     Hτ  = copy(H)
-    Hτ2  = copy(H)
+    Hτ2 = copy(H)
     size_H1_2, size_H2_2, size_H3_2 = size(H,1)-2, size(H,2)-2, size(H,3)-2
+    R_H = @zeros(nx-2, ny-2, nz-2) # residual without halo
+    R_v = zeros(nx_v, ny_v, nz_v)  # global residual
 
     t_tic = 0.0; niter = 0
     # Dual-time stepping
-    for it = 1:nt
+    # physical time stepping
+    for it = 1:nt 
         if (it==11) t_tic = Base.time(); niter = 0 end
-        # @hide_communication (8, 2) begin
-            @parallel compute!(C2, C, D_dx, D_dy, dt, _dx, _dy, size_C1_2, size_C2_2)
-            C, C2 = C2, 
-            update_halo!(C)
-        # end
-        niter += 1
-
-        # Visualize
-        if do_visu && (it % nout == 0)
-            C_inn .= C[2:end-1,2:end-1]; gather!(C_inn, C_v)
-            if (me==0)
-                opts = (aspect_ratio=1, xlims=(Xi_g[1], Xi_g[end]), ylims=(Yi_g[1], Yi_g[end]), clims=(0.0, 1.0), c=:davos, xlabel="Lx", ylabel="Ly", title="time = $(round(it*dt, sigdigits=3))")
-                heatmap(Xi_g, Yi_g, Array(C_v)'; opts...); frame(anim)
-            end
+        # pseudo time stepping
+        R_m = 1.0; iter = 0
+        while R_m > tol
+            @parallel compute_pseudo!(Hτ2, Hτ, H, D_dx, D_dy, D_dz, dt, dτ, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
+            Hτ, Hτ2 = Hτ2, Hτ
+            update_halo!(Hτ)
+            # compute residuals
+            @parallel compute_residuals!(R_H, Hτ, H, D_dx, D_dy, D_dz, dt, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
+            gather!(R_H, R_v) 
+            R_m  = maximum(R_v) 
+            iter += 1
         end
+        # update H 
+        H[:,:,:] = Hτ; niter += 1
     end
 
     t_toc = Base.time() - t_tic
