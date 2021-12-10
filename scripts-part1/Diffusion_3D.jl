80a388c4f36519a9a1a8e6497fa0f3b5d29cbaef
ywang_octopus <yijwang@student.ethz.ch>
2021-12-10 11:36:43 +0100

    Added reference test for Diffusion_3D

--- scripts-part1/Diffusion_3D.jl
+++ scripts-part1/Diffusion_3D.jl
@@ -2,9 +2,9 @@ const USE_GPU = false
 using ParallelStencil
 using ParallelStencil.FiniteDifferences3D
 @static if USE_GPU
-    @init_parallel_stencil(CUDA, Float64, 2)
+    @init_parallel_stencil(CUDA, Float64, 3)
 else
-    @init_parallel_stencil(Threads, Float64, 2)
+    @init_parallel_stencil(Threads, Float64, 3)
 end
 using Plots, Printf, ImplicitGlobalGrid
 import MPI
@@ -16,16 +16,14 @@ macro qz(ix,iy,iz)  esc(:( -D_dz*(Hτ[$ix+1,$iy+1,$iz+1] - Hτ[$ix+1,$iy+1,$iz  
 
 @parallel_indices (ix,iy,iz) function compute_pseudo!(Hτ2, Hτ, H, D_dx, D_dy, D_dz, dt, dτ, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
     if (ix<=size_H1_2 && iy<=size_H2_2 && iz<=size_H3_2)
-        Hτ2[ix+1,iy+1,iz+1] = Hτ[ix+1,iy+1,iz+1] - dτ*( (Hτ[ix+1,iy+1,iz+1] - H[ix+1,iy+1,iz+1])/dt 
-                            + (@qx(ix+1,iy,iz)-@qx(ix,iy,iz))*_dx + (@qy(ix,iy+1,iz)-@qy(ix,iy,iz))*_dy + (@qz(ix,iy,iz+1)-@qz(ix,iy,iz))*_dz )
+        Hτ2[ix+1,iy+1,iz+1] = Hτ[ix+1,iy+1,iz+1] - dτ*( (Hτ[ix+1,iy+1,iz+1] - H[ix+1,iy+1,iz+1])/dt + (@qx(ix+1,iy,iz)-@qx(ix,iy,iz))*_dx + (@qy(ix,iy+1,iz)-@qy(ix,iy,iz))*_dy + (@qz(ix,iy,iz+1)-@qz(ix,iy,iz))*_dz )
     end
     return
 end
 
 @parallel_indices (ix,iy,iz) function compute_residuals!(R_H, Hτ, H, D_dx, D_dy, D_dz, dt, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
     if (ix<=size_H1_2 && iy<=size_H2_2 && iz<=size_H3_2)
-        R_H[ix,iy,iz] = -(Hτ[ix+1,iy+1,iz+1] - H[ix+1,iy+1,iz+1])/dt 
-                        - (@qx(ix+1,iy,iz)-@qx(ix,iy,iz))*_dx - (@qy(ix,iy+1,iz)-@qy(ix,iy,iz))*_dy - (@qz(ix,iy,iz+1)-@qz(ix,iy,iz))*_dz
+        R_H[ix,iy,iz] = -(Hτ[ix+1,iy+1,iz+1] - H[ix+1,iy+1,iz+1])/dt - (@qx(ix+1,iy,iz)-@qx(ix,iy,iz))*_dx - (@qy(ix,iy+1,iz)-@qy(ix,iy,iz))*_dy - (@qz(ix,iy,iz+1)-@qz(ix,iy,iz))*_dz
     end
     return
 end
@@ -39,7 +37,6 @@ end
 
     # Numerics
     nx, ny, nz = 32, 32, 32
-    nout       = 1
     tol        = 1e-8
 
     # Derived numerics
@@ -48,24 +45,28 @@ end
       dx,   dy,   dz = Lx/nx_g(), Ly/ny_g(), Lz/nz_g()
      _dx,  _dy,  _dz = 1.0/dx, 1.0/dy, 1.0/dz
     D_dx, D_dy, D_dz = D/dx, D/dy, D/dz
-    nx_v, ny_v, nz_v = (nx-2)*dims[1], (ny-2)*dims[2], (nz-2)*dims[3]  # global dimensions
-                  dτ = min(dx, dy, dz)^2/D/4.5
+    nx_v, ny_v, nz_v = (nx-2)*dims[1], (ny-2)*dims[2], (nz-2)*dims[3]  
+                  dτ = min(dx, dy, dz)^2/D/8.0
                   nt = cld(ttot, dt)
     
     # Array initialisation
     H   = @zeros(nx, ny, nz)
-    H  .= Data.Array([exp(-(x_g(ix,dx,H)+dx/2 -Lx/2)^2-(y_g(iy,dy,H)+dy/2 -Ly/2)^2-(z_g(iz,dz,H)+dz/2 -Lz/2)^2) for ix=1:size(H,1), iy=1:size(H,2), iz=1:size(H,3)])
+    H  .= Data.Array([2.0*exp(-((x_g(ix,dx,H)+dx/2 -Lx/2)^2+(y_g(iy,dy,H)+dy/2 -Ly/2)^2+(z_g(iz,dz,H)+dz/2 -Lz/2)^2)/2) for ix=1:size(H,1), iy=1:size(H,2), iz=1:size(H,3)])
     Hτ  = copy(H)
     Hτ2 = copy(H)
     size_H1_2, size_H2_2, size_H3_2 = size(H,1)-2, size(H,2)-2, size(H,3)-2
-    R_H = @zeros(nx-2, ny-2, nz-2) # residual without halo
-    R_v = zeros(nx_v, ny_v, nz_v)  # global residual
+    R_H = @zeros(nx-2, ny-2, nz-2)  # residual without halo
+    R_g = zeros(nx_v, ny_v, nz_v)   # global residual
+    H_g = zeros(nx_v, ny_v, nz_v)   # global H without boundaries
+    H_inn = zeros(nx-2, ny-2, nz-2) # no halo local array 
 
-    t_tic = 0.0; niter = 0
-    # Dual-time stepping
+    # global coordinates: x, y, inner points only
+    xc_g, yc_g = LinRange(dx+dx/2, Lx-dx-dx/2, nx_v), LinRange(dy+dy/2, Ly-dy-dy/2, ny_v) 
+
     # physical time stepping
+    t_tic = 0.0; niter = 0
     for it = 1:nt 
-        if (it==11) t_tic = Base.time(); niter = 0 end
+        # if (it==11) t_tic = Base.time(); niter = 0 end
         # pseudo time stepping
         R_m = 1.0; iter = 0
         while R_m > tol
@@ -74,21 +75,32 @@ end
             update_halo!(Hτ)
             # compute residuals
             @parallel compute_residuals!(R_H, Hτ, H, D_dx, D_dy, D_dz, dt, _dx, _dy, _dz, size_H1_2, size_H2_2, size_H3_2)
-            gather!(R_H, R_v) 
-            R_m  = maximum(R_v) 
+            gather!(Array(R_H), R_g) 
+            R_m  = maximum(R_g) 
             iter += 1
+            # print residuals
+            # if iter % 20 == 0 && me == 0
+            #     println("time = $(round(it*dt, sigdigits=3))s, iterations = $(iter), residual = $(R_m)")
+            # end
         end
         # update H 
         H[:,:,:] = Hτ; niter += 1
+        # gather global array
+        H_inn .= Array(H[2:end-1,2:end-1,2:end-1]); gather!(H_inn, H_g)
+        # Visualize
+        if do_visu && me==0
+            opts = (aspect_ratio=1, xlims=(xc_g[1], xc_g[end]), ylims=(yc_g[1], yc_g[end]), clims=(0.0, 1.0), c=:davos, xlabel="Lx", ylabel="Ly", title="time = $(round(it*dt, sigdigits=3))")
+            display(heatmap(xc_g, yc_g, Array(H_g[:,:,15])'; opts...))
+        end
     end
 
-    t_toc = Base.time() - t_tic
-    A_eff = 2/1e9*nx_g()*ny_g()*sizeof(Float64)  # Effective main memory access per iteration [GB]
-    t_it  = t_toc/niter                          # Execution time per iteration [s]
-    T_eff = A_eff/t_it                           # Effective memory throughput [GB/s]
+    # t_toc = Base.time() - t_tic
+    # A_eff = 2/1e9*nx_g()*ny_g()*nz_g()*sizeof(Float64)  # Effective main memory access per iteration [GB]
+    # t_it  = t_toc/niter                          # Execution time per iteration [s]
+    # T_eff = A_eff/t_it                           # Effective memory throughput [GB/s]
 
     finalize_global_grid()
-    return
+    return Array(H_g), xc_g
 end
 
-diffusion_3D(; do_visu=true)
+H_g, xc_g = diffusion_3D(; do_visu=false);
--- test/part1.jl
+++ test/part1.jl
@@ -1,16 +1,15 @@
 # Testing part 1
+using Test, ReferenceTests
 
-include("../scripts-part1/part1.jl") # modify to include the correct script
-
-# Add unit and reference tests
-
+# dir to the part 1 script
+include("../scripts-part1/Diffusion_3D.jl") 
 
 # Reference test using ReferenceTests.jl
 "Compare all dict entries"
 comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (v1,v2) in zip(values(d1), values(d2))])
 inds = Int.(ceil.(LinRange(1, length(xc_g), 12)))
-d = Dict(:X=> xc_g[inds], :H=>H[inds, inds, 15])
+d = Dict(:X=> xc_g[inds], :H=>H_g[inds, inds, 15])
 
-@testset "Ref-file" begin
-    @test_reference "reftest-files/test_1.bson" d by=comp
+@testset "Reference Test" begin
+    @test_reference "reftest-files/test_part_1.bson" d by=comp
 end
